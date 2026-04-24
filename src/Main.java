5c1a5647b48aecf8dd0fb698214847d93a22873f
IrinaAntonova0 <irina.antonova.dipl.inf@gmail.com>
2026-04-24 10:59:58 +0200

    add .gitignore

---
+++ src/Main.java
@@ -0,0 +1,68 @@
+public class Main {
+    //sichtbarkeit static void {name der methode} (Übergabeparameter)
+    /*static void main() {
+
+    }*/
+    public static void main (String[] args){
+        System.out.println("Hello Creative World!");//sout -> System.out.println();
+        //Datentypen
+        //primitive Datentypen
+        //Ganzzahlen
+        byte byte1 = 1;//-128 - 127 8bit
+        short short1 = 1;//-32.768 - 32.767 16bit
+        /** Default **/
+        int int1 =1;//-2.147.483.648 - 2.147.483.647 32bit
+        long long1 = 1l;// .... 64bit
+
+        //Fließkommazahlen
+        float float1 = 1.0f;//32bit
+        /** Default **/
+        double double1 =1.0;//64bit
+
+        //Wahrheitswerte
+        boolean bool1 = true;
+        boolean bool2 = false;
+        //Zeichen
+        char char1  = 'a';
+
+        //komplexe Datentypen
+        String str1 = "Hello World!";
+
+        //Operatoren
+        //mathematische
+        int a = 2;
+        int b = 5;
+        int sum = a + b;
+        int diff = b - a;
+        int prod = a * b;
+        int quotient = b / a;
+        double quotientD = (double) b / a;//TypeCasting
+        System.out.println("sum: " + sum);
+        System.out.println("diff: " + diff);
+        System.out.println("prod: " + prod);
+        System.out.println("quotient: "+ quotient);
+        System.out.println("quotientDCast: "+ quotientD);
+
+        //vergleichende
+        int c = 4;
+        int d = 1;
+        boolean isEqual = c == d;
+        boolean isGreater = c > d;
+        boolean isLessOrEqual = c <= d;
+        System.out.println("isEqual: "+isEqual);
+        System.out.println("isLessOrEqual: "+isLessOrEqual);
+        //logische
+        /**
+         * Und OP &&
+         * Oder OP ||
+         * Not OP !
+         */
+
+        //Zeichen
+        String str2 = new String("str2");
+        String str3 = new String("str2");
+        System.out.println("Check string equals: " + str2 == str3);
+        System.out.println("Check string equals: " +  (str2.equals(str3)));
+
+    }//<--up to here
+}
---
+++ src/MainScanner.java
@@ -0,0 +1,68 @@
+import java.util.Scanner;
+
+public class MainScanner {
+    static void main(String[] args) {
+        //Aufgabe 8
+/*        Scanner sc8 = new Scanner(System.in);
+
+        System.out.println("Please enter your Name: ");
+
+        String input1 = sc8.nextLine();
+
+        System.out.println("Good morning " + input1+" !" );
+
+        sc8.close();
+*/
+        //Aufgabe 9
+/*        Scanner sc9 = new Scanner(System.in);
+
+        System.out.println("Please enter your Name: ");
+        String name = sc9.nextLine();
+        System.out.println("Please enter your Age: ");
+        int alter = sc9.nextInt();
+
+        System.out.println("---------------------------------");
+        System.out.printf("%-20s | %10s%n" ,"Name" , "Alter" );
+        System.out.println("---------------------------------");
+        System.out.printf("%-20s | %10s%n" , name , alter );
+        System.out.println("---------------------------------");
+
+        sc9.close();
+*/
+        //Aufgabe 10
+        Scanner sc10 = new Scanner(System.in);
+
+        System.out.println("Please enter your Name: ");
+        String name10 = sc10.nextLine();
+        System.out.println("Please enter your Age: ");
+        int alter10 = sc10.hasNextInt() ? sc10.nextInt() : 0;
+
+        sc10.nextLine();
+
+        System.out.println("Please state your favourite saying: ");
+        String saying10 = sc10.nextLine();
+
+        System.out.println("Please enter a operation(+ or -), followed by several intergers(one integer per line), delimiter is space, when finished type 'END': ");
+
+        String op10 = sc10.next().equals("+") ? "+" : "-";
+        int result = 0;
+        while ( sc10.hasNextInt()){
+            if(op10.equals("+"))
+                result += sc10.nextInt();
+            else
+                result -= sc10.nextInt();
+        }
+
+        System.out.println("---------------------------------");
+        System.out.printf("%-20s | %10s%n" ,"Name" , "Alter" );
+        System.out.println("---------------------------------");
+        System.out.printf("%-20s | %10s%n" ,name10 , alter10 );
+        System.out.println("---------------------------------");
+
+        System.out.println("Favourite Saying: "  + saying10);
+
+        System.out.println("Result of "+op10+" ~ing all Integers: "+ result);
+
+        sc10.close();
+    }
+}
